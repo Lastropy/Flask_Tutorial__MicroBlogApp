4e5e65b1c1e8208ab3a6595423885fa8adee6747
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2025-06-11 15:18:52 +0530

    Added login functionity with form validations

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,flash, redirect, url_for
 from app import app
+from app.forms.loginform import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form_obj = LoginForm()
+    if login_form_obj.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            login_form_obj.username.data, login_form_obj.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=login_form_obj)
